5fe671eb51874d36607ffaa208ce79e3bb9afd1b
Hiperultimate <hiperultimatevegito@gmail.com>
2021-03-08 00:06:35 +0530

    Optimized Icon display code.

--- logic.py
+++ logic.py
@@ -3,51 +3,17 @@ from tkinter import ttk
 
 from search_logic import search_dict
 
-from win32com.shell import shell, shellcon  
 import win32api
-import win32con
-import win32ui  
+import win32con 
 import win32gui
 
-from PIL import Image, ImageTk 
+from PIL import ImageTk 
 
 ################################################################################################################
 #FOR TESTING
 def printsome():
     print("henlo")
 ################################################################################################################
-
-def get_icon(PATH, size):  
-    SHGFI_ICON = 0x000000100  
-    SHGFI_ICONLOCATION = 0x000001000  
-    if size == "small":  
-        SHIL_SIZE= 0x00001  
-    elif size == "large":  
-        SHIL_SIZE= 0x00002  
-    else:  
-        raise TypeError("Invalid argument for 'size'. Must be equal to 'small' or 'large'")  
-    ret, info = shell.SHGetFileInfo(PATH, 0, SHGFI_ICONLOCATION | SHGFI_ICON | SHIL_SIZE)  
-    hIcon, iIcon, dwAttr, name, typeName = info  
-    ico_x = win32api.GetSystemMetrics(win32con.SM_CXICON)  
-    hdc = win32ui.CreateDCFromHandle(win32gui.GetDC(0))  
-    hbmp = win32ui.CreateBitmap()  
-    hbmp.CreateCompatibleBitmap(hdc, ico_x, ico_x)  
-    hdc = hdc.CreateCompatibleDC()  
-    hdc.SelectObject(hbmp)  
-    hdc.DrawIcon((0, 0), hIcon)  
-    win32gui.DestroyIcon(hIcon)  
-    
-    bmpinfo = hbmp.GetInfo()  
-    bmpstr = hbmp.GetBitmapBits(True)  
-    img = Image.frombuffer(  
-        "RGBA",  
-        (bmpinfo["bmWidth"], bmpinfo["bmHeight"]),  
-        bmpstr, "raw", "BGRA", 0, 1  
-    )  
-    
-    if size == "small":  
-        img = img.resize((16, 16), Image.ANTIALIAS)  
-    return img  
 
 def updateScrollRegion(main_frame, my_canvas):
 	main_frame.update_idletasks()
@@ -88,8 +54,10 @@ def search_result(top, search_string, my_frame, main_frame):
 
     dict_items = search_dict(search_string)
     key_items = list(dict_items)
+    
     for row in range(len(dict_items.items())):
-        make_button(my_frame, key_items[row],dict_items[key_items[row]], row)
+        # print(dict_items,row ,'\n')
+        make_button(my_frame, key_items[row],dict_items[key_items[row]]['location'],dict_items[key_items[row]]['icon'], row)
 
     updateScrollRegion(main_frame, my_frame)
 
@@ -99,14 +67,11 @@ def on_enter(button_name):
 def on_leave(button_name):
     button_name['background'] = "#171717"
 
-#Make sure the icon location object exists 
-    #Maybe its better to save the icons and load them here
-images = []
-def make_button(widget,item_name,location,row_):
-
-    icon = ImageTk.PhotoImage(get_icon(location,"large"))
-    images.append(icon)
-    print(type(icon), location)
+#Here images is used to store the references of icons. Without it icons will not be displayed.
+images = set()
+def make_button(widget,item_name,location,icon,row_):
+    icon = ImageTk.PhotoImage(icon)
+    images.add(icon)
 
 
     item_name = item_name.ljust(600, " ")
--- search_logic.py
+++ search_logic.py
@@ -3,6 +3,14 @@ import os
 import glob
 import pickle
 
+from win32com.shell import shell, shellcon  
+import win32api
+import win32con
+import win32ui  
+import win32gui
+
+from PIL import Image, ImageTk 
+
 """
 Program Search Display Priority Scaling:
         1 - Highest Priority
@@ -25,6 +33,38 @@ Program Search Display Priority Scaling:
 #     # print(app.name)
 #     print(app)
 
+def get_icon(PATH, size):  
+    SHGFI_ICON = 0x000000100  
+    SHGFI_ICONLOCATION = 0x000001000  
+    if size == "small":  
+        SHIL_SIZE= 0x00001  
+    elif size == "large":  
+        SHIL_SIZE= 0x00002  
+    else:  
+        raise TypeError("Invalid argument for 'size'. Must be equal to 'small' or 'large'")  
+    ret, info = shell.SHGetFileInfo(PATH, 0, SHGFI_ICONLOCATION | SHGFI_ICON | SHIL_SIZE)  
+    hIcon, iIcon, dwAttr, name, typeName = info  
+    ico_x = win32api.GetSystemMetrics(win32con.SM_CXICON)  
+    hdc = win32ui.CreateDCFromHandle(win32gui.GetDC(0))  
+    hbmp = win32ui.CreateBitmap()  
+    hbmp.CreateCompatibleBitmap(hdc, ico_x, ico_x)  
+    hdc = hdc.CreateCompatibleDC()  
+    hdc.SelectObject(hbmp)  
+    hdc.DrawIcon((0, 0), hIcon)  
+    win32gui.DestroyIcon(hIcon)  
+    
+    bmpinfo = hbmp.GetInfo()  
+    bmpstr = hbmp.GetBitmapBits(True)  
+    img = Image.frombuffer(  
+        "RGBA",  
+        (bmpinfo["bmWidth"], bmpinfo["bmHeight"]),  
+        bmpstr, "raw", "BGRA", 0, 1  
+    )  
+    
+    if size == "small":  
+        img = img.resize((16, 16), Image.ANTIALIAS)  
+    return img  
+
 def windows_search_startmenu():
     """
     For Start Menu Search Only
@@ -42,8 +82,11 @@ def windows_search_startmenu():
     for shortcuts in all_shortcut_list:
         name = os.path.basename(os.path.normpath(shortcuts)[:-4])
         location = os.sep.join([search_location, str(shortcuts)])
+        icon = get_icon(location,"large")
         applications_dict[name] = {}
         applications_dict[name]['location'] =  location
+        applications_dict[name]['icon'] =  icon
+
 
     dump_tofile = open(current_directory+'\\search_collection.bin', 'ab') 
     pickle.dump(applications_dict, dump_tofile)
@@ -62,7 +105,7 @@ def search_dict(search_string):
     print(os.getcwd())
     dict_file = open("search_collection.bin", "rb")
     applications_dict = pickle.load(dict_file)
-    search_result = {keys : applications_dict[keys]["location"] for keys in applications_dict.keys() if search_string.lower() in keys.lower()}
+    search_result = {keys : {'location':applications_dict[keys]["location"], 'icon': applications_dict[keys]["icon"] } for keys in applications_dict.keys() if search_string.lower() in keys.lower()}
     return(search_result)
 
     # for keys in applications_dict.keys():
@@ -71,5 +114,5 @@ def search_dict(search_string):
     # search_result = [(keys, applications_dict[keys]["location"]) for keys in applications_dict.keys() if search in keys]
 
 
-# windows_search_startmenu()
+windows_search_startmenu()
 # search_dict("valo")
